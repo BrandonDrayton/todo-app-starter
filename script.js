70beb59763bbb120806d85efee2211fa93d514cb
Brandon Drayton <brandondrayton@gmail.com>
2022-04-27 09:56:17 -0400

    Tried till midnight could not work in checkbox

--- script.js
+++ script.js
@@ -2,5 +2,26 @@
 // comes before 'script.js' in the HTML document
 console.log(todoItems)
 
+const completedList = document.querySelector("#complete-items")
+const uncompletedList = document.querySelector("#incomplete-items")
+// const checkMark = document.getElementById("#flexCheckChecked")
+
+for (let item of todoItems) {
+    const currentItem = document.createElement("li")
+    currentItem.innerText = item.title
+    currentItem.classList.add("form-check")
+    currentItem.classList.add("list-group-item")
+    if (item.completed === true) {
+        completedList.appendChild(currentItem)
+        currentItem.ID = "#flexCheckChecked"
+
+    }
+    else {
+        uncompletedList.appendChild(currentItem)
+    }
+}
+
+
+
 // Use the 'todoItems' variable to create the appropriate elements and append them onto
 // the page.
